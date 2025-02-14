9e08b48ec662a1003582c8118f0075b10f8d928d
Daniel Rubin <daniel.rubin1997@gmail.com>
2025-02-14 20:15:06 +0100

    fix: make page dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,12 @@ type Coin = {
       "marketCapRank": number,
       "priceChangePercentage7dInCurrency": number,
       "image"?: string
-    }
+    };
+
+export const dynamic = 'force-dynamic';
 
 export default async function Home() {
-  const response = await fetch(`${process.env.API_URL}/coins`);
+  const response = await fetch(`${process.env.API_URL}/coins`, { next: { revalidate: 0 } });
 
   if (response.status !== 200) {
       return <div>There was an error fetching the coin-data: {response.status} {response.statusText}</div>;
