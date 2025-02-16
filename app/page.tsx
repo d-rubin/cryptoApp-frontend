f3f155041136662e78454f2496c29c8f3735e032
Daniel Rubin <daniel.rubin1997@gmail.com>
2025-02-16 10:56:28 +0100

    fix: make page dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,30 @@
 import {CryptoCurrencyCard} from "@/components/crypto-currency-card";
+import {revalidatePath} from "next/cache";
 
 type Coin = {
-      "id": number,
-      "coinId": string,
-      "symbol": string,
-      "name": string,
-      "currentPrice": number,
-      "marketCapRank": number,
-      "priceChangePercentage7dInCurrency": number,
-      "image"?: string
-    };
-
-export const dynamic = 'force-dynamic';
+    "id": number,
+    "coinId": string,
+    "symbol": string,
+    "name": string,
+    "currentPrice": number,
+    "marketCapRank": number,
+    "priceChangePercentage7dInCurrency": number,
+    "image"?: string
+};
 
 export default async function Home() {
-  const response = await fetch(`${process.env.API_URL}/coins`, { next: { revalidate: 0 } });
+    revalidatePath('/coins', "page");
+    const response = await fetch(`${process.env.API_URL}/coins`);
 
-  if (response.status !== 200) {
-      return <div>There was an error fetching the coin-data: {response.status} {response.statusText}</div>;
-  }
+    if (response.status !== 200) {
+        return <div>There was an error fetching the coin-data: {response.status} {response.statusText}</div>;
+    }
 
-  const json: { coins: Array<Coin>} = await response.json();
+    const json: { coins: Array<Coin> } = await response.json();
 
-  return json.coins.map((coin) => {
-      return <CryptoCurrencyCard key={coin.coinId} name={coin.name} symbol={coin.symbol} currentPrice={coin.currentPrice} priceChange7d={coin.priceChangePercentage7dInCurrency} image={coin.image} />
-  })
+    return json.coins.map((coin) => {
+        return <CryptoCurrencyCard key={coin.coinId} name={coin.name} symbol={coin.symbol}
+                                   currentPrice={coin.currentPrice}
+                                   priceChange7d={coin.priceChangePercentage7dInCurrency} image={coin.image}/>
+    })
 }
